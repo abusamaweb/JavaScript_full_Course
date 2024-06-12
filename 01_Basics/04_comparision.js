555516fd519a737d757d53a3863c71205f0249d2
Abu shama <100144949+abusamaweb@users.noreply.github.com>
2024-06-12 00:55:43 +0000

    compare

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,27 @@
+
+console.log(3 > 2); // true 
+console.log(3 < 2); // false
+console.log(3 >= 2); // true
+console.log(3 <= 2); // false
+console.log(3 == 3); // true
+console.log(3 === 3); // true
+console.log(3 != 3); // false
+console.log(3 !== 3); // false
+
+console.log("2" > 1); // true
+console.log("2" < 1); // false
+
+console.log(null < 0); // false
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+
+console.log(undefined < 0); // false
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+
+// strict check:-
+console.log( "2" == 2); // true
+console.log( "2" === 2); // false // strict check
