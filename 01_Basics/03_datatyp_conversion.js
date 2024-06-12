5b4a8b8886ac07c6a74091f6acb7d23bbe0f5250
Abu shama <100144949+abusamaweb@users.noreply.github.com>
2024-06-12 00:45:56 +0000

    Operations discussed

---
+++ 01_Basics/03_datatyp_conversion.js
@@ -0,0 +1,74 @@
+// let score = 33;
+// let score = "abushama";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLogged = "abu";
+
+// let isLoggedIn = Boolean(isLogged);
+// console.log(isLoggedIn);
+
+// 0 => false
+// 1 => true
+// "" => false
+// "abu" => true
+
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ========= Operation ========= //
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(3 + 3);
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 ** 3);
+// console.log(3 / 3);
+// console.log(3 % 3);
+
+// contatination:-
+// let str1 = "hello";
+// let str2 = " world";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log( "33" + 3); // 333
+// console.log( 33 + "3"); // 333
+
+// console.log("1" + 2 + 3); // 123
+// console.log(1 + 2 + "3"); // 33
+
+// console.log((4 + 7) * 5 % 6); // 5
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+// console.log(+"abu"); // NaN
+
+// let num1 , num2 , num3;
+// num1 = num2 = num3 = 33;
+// console.log(num1, num2, num3);
+
+// let gameCounter = 100;
+// // gameCounter ++;
+// ++gameCounter;
+// console.log(gameCounter);
+
+
